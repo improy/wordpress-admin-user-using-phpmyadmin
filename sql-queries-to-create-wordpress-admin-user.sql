5de3f174b9001d98a818dce6b1643359d31ceb57
PRoy <imraviroy@users.noreply.github.com>
2017-01-08 03:40:50 +0600

    Create sql-queries-to-create-wordpress-admin-user.sql

---
+++ sql-queries-to-create-wordpress-admin-user.sql
@@ -0,0 +1,24 @@
+-- phpMyAdmin SQL Dump
+-- Create A WordPress Admin User using SQL Queries in MySQL Database
+-- assuming database table prefix is set to “wp_”
+-- Read more at: http://www.proy.info/create-a-wordpress-admin-user-via-mysql-database-using-phpmyadmin/ 
+
+-- 
+-- INSERT QUERY
+-- Table: `wp_users`
+--
+
+-- --------------------------------------------------------
+INSERT INTO `your_wp_database`.`wp_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`) 
+VALUES (NULL, 'newuser', MD5('new-password'), 'Site Admin', 'newuser@yourdomain.com', 'http://www.yourdomain.com', '2017-01-08 00:00:00', '', '0', 'New Admin');
+
+-- 
+-- INSERT QUERY
+-- Table: `wp_usermeta`
+--
+
+-- --------------------------------------------------------
+INSERT INTO `your_wp_database`.`wp_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
+VALUES 
+(NULL, '2', 'wp_capabilities', 'a:1:{s:13:"administrator";s:1:"1";}'), 
+(NULL, '2', 'wp_user_level', '10');
